ae9344e138ad14e7005c0d7c2a315cace486eafc
RIOKOWI <achyarrio2004@gmail.com>
2025-11-07 11:09:07 +0700

    masukkan gambar di splash pertamaa

--- lib/screen/splash_screen1.dart
+++ lib/screen/splash_screen1.dart
@@ -5,6 +5,30 @@ class Splash1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+            appBar: AppBar(
+              actions: const [],
+            ),
+            body: Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 50,),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 25),
+                    width: 250,
+                    height: 250,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                      image: DecorationImage(
+                        image: AssetImage('assets/image/images(1)'),
+                        fit: BoxFit.cover
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
   }
 }
