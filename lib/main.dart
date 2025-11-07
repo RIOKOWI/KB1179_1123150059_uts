eab9e6d4818af785ee84a6c28c088fcbb4e81b8c
RIOKOWI <achyarrio2004@gmail.com>
2025-11-07 10:53:40 +0700

    buat code sesuai soal

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,24 @@
 import 'package:flutter/material.dart';
 
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NIM : 1123150059, Rio Achyar',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.red
+        ),
+      ),
+      
+    );
+  }
+}
