eb0c09a7285d6c731fd5b5c19e577974ecbd58cb
RIOKOWI <achyarrio2004@gmail.com>
2025-11-08 06:32:15 +0700

    buat text field login

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -6,6 +6,7 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red,
             body: Center(
               child: Column(
                 children: [
@@ -13,21 +14,60 @@ class Login extends StatelessWidget {
                     Icon(
                       Icons.key,
                       size: 80,
-                      color: Colors.red,
+                      color: Colors.white,
                     ),
 
                     SizedBox(height: 20,),
                   Text("Welcome", 
                     style: TextStyle(
                       fontSize: 30,
-                      fontWeight: FontWeight.bold
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
                     ),
                   ),
                   SizedBox(height: 5,),
                   Text("Please Login to Enter the Application", 
                     style: TextStyle(
                       fontSize: 15,
-                      color: Colors.red
+                      color: Colors.white
+                    ),
+                  ),
+
+                  SizedBox(height: 20,),
+                  TextField(
+                    style: TextStyle(
+                      color: Colors.white
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                        color: Colors.white
+                      ),
+                      hintText: 'rio@gmail.com',
+                      hintStyle: TextStyle(
+                        color: Colors.white
+                      ),
+                      prefixIcon: 
+                      const Icon(
+                        Icons.email,
+                        size: 24.0,
+                        color: Colors.white,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.white)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 2
+                        ),
+                      ),
                     ),
                   ),
                 ],
